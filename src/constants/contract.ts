d316b8bc4666252dd6712bb160f3fbb7e8f639b5
metaflowx <metaflowx@gmail.com>
2025-04-23 00:51:00 +0530

    Update contract.ts for token & vc contract

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -3,11 +3,11 @@ import { RefferABI } from "@/app/ABI/RewardToken";
 import { StakingABI } from "@/app/ABI/StakingABI";
 import { TokenABI } from "@/app/ABI/TokenSupply";
 import { VCABI } from "@/app/ABI/VCABI";
-export const TokenContractAddress = "0x8f8eb871f072ed73dc592a7201bae514e08c9f3f"; 
+export const TokenContractAddress = "0xebfa9073A0fA6d440185352175ce1a625FbBf819"; 
 export const ICOContractAddress ="0x531135F57b9490c10Caa8C3CA3C667b45D7017E0"
 export const ReferralContractAddress="0x20d30fe1e37df13a9eff393863617b3ec9146144"
-export const VCContractAddress="0x4Ab65FE359DCa48790a5c4D32Ba3d3050134A32e"
-export const USDTAddress="0xE394f7359E0FD4f82EFDe5c3eD0CAac3172276f2"
+export const VCContractAddress="0xAFb5De95758bD9d818c52cd037d8a856aFBEE914"
+export const USDTAddress="0x55d398326f99059fF775485246999027B3197955"
 export const StakeContractAddress="0x8E8a74264d47334a6c3273a7BDAB587544d9Cdbd"
 
 
