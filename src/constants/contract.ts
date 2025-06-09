af9e8947d8b540fc8fac1402528e643089869cb7
mdsac1441 <amdahmed0@gmail.com>
2025-06-09 20:39:49 +0530

    testnet address change

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -4,11 +4,11 @@ import { StakingABI } from "@/app/ABI/StakingABI";
 import { TokenABI } from "@/app/ABI/TokenSupply";
 import { VCABI } from "@/app/ABI/VCABI";
 export const TokenContractAddress = "0xebfa9073A0fA6d440185352175ce1a625FbBf819"; 
-export const ICOContractAddress ="0x531135F57b9490c10Caa8C3CA3C667b45D7017E0"
-export const ReferralContractAddress="0x20d30fe1e37df13a9eff393863617b3ec9146144"
+export const ICOContractAddress ="0xa4A84814a48eb0caE1d1C7531BDdD27f135FCdEb"
+export const ReferralContractAddress="0xA86d21673e1810224113b7805FEAc494835F9e3a"
 export const VCContractAddress="0xAFb5De95758bD9d818c52cd037d8a856aFBEE914"
 export const USDTAddress="0x55d398326f99059fF775485246999027B3197955"
-export const StakeContractAddress="0x8E8a74264d47334a6c3273a7BDAB587544d9Cdbd"
+export const StakeContractAddress="0x701c084E2Ee6a38A926c11D4dDCCbCDc54b533B0"
 
 
 export const contractConfig = {
